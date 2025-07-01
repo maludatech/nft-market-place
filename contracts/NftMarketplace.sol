9d096eb9af4ac8f5689a4d25de8f647e965e2bc1
maluda <nwitevictor7@gmail.com>
2025-07-01 15:36:58 +0100

    updated contract/NFTMarketPlace.sol

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -9,18 +9,40 @@ import {
 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTMarketPlace is ERC721URIStorage {
-    uint256 private _tokenId;
+    uint256 private _tokenIds;
     uint256 private _itemsSold;
 
     address payable owner;
 
-    constructor() ERC721("NFTMarketPlace", "NMP") {}
-
     mapping(uint256 => MarketItem) private idMarketItem;
 
     struct MarketItem {
+        uint256 tokenId;
         address payable seller;
+        address payable owner;
         uint256 price;
         bool sold;
     }
+
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "only owner of the marketplace can change the listing price");
+        _;
+    }
+
+    constructor() ERC721("NFT Metaverse Token", "NMT") {
+        owner == payable(msg.sender);
+    }
+
+    function updateListingPrice(uint256 _listingPrice) public payable{
+        
+    }
+    
 }
